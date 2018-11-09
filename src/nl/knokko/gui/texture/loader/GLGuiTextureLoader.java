0562e0ca094a8be4449928c457a88871b8ce59a6
knokko <knokogator@hotmail.com>
2018-11-09 10:40:38 +0100

    Handle any image size in GLGuiTextureLoader and fix bug when loading BufferedImage with specified bounds

--- src/nl/knokko/gui/texture/loader/GLGuiTextureLoader.java
+++ src/nl/knokko/gui/texture/loader/GLGuiTextureLoader.java
@@ -47,7 +47,8 @@ public class GLGuiTextureLoader implements GuiTextureLoader {
 	private PrintStream errorOutput;
 	
 	private static final int[] POWERS = {
-		1,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576
+		1,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,
+		2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824
 	};
 	
 	private static boolean isPowerOf2(int size){
@@ -56,63 +57,81 @@ public class GLGuiTextureLoader implements GuiTextureLoader {
 				return true;
 		return false;
 	}
+	
+	private static int next2Power(int number) {
+		for (int index = 0; index < POWERS.length; index++)
+			if (POWERS[index] >= number)
+				return POWERS[index];
+		throw new IllegalArgumentException("Number too big: " + number);
+	}
 
 	public GLGuiTextureLoader() {
 		textures = new ArrayList<Integer>();
 		errorOutput = System.out;
 	}
+	
 	@Override
 	public GuiTexture loadTexture(BufferedImage source, int minX, int minY, int maxX, int maxY) {
-		boolean allowAlpha = source.getTransparency() != BufferedImage.OPAQUE;
-		int effWidth = maxX - minX + 1;
-		int effHeight = maxY - minY + 1;
-		ByteBuffer buffer = BufferUtils.createByteBuffer(effWidth * effHeight * (allowAlpha ? 4 : 3)); //4 for RGBA, 3 for RGB
-	    for(int y = minY; y <= maxY; y++){
-	        for(int x = minX; x <= maxX; x++){
-	        	int rgb = source.getRGB(x, y);
-	        	buffer.put((byte) (rgb >> 16));
-	        	buffer.put((byte) (rgb >> 8));
-	        	buffer.put((byte) (rgb >> 0));
-	        	if(allowAlpha)
-	        		buffer.put((byte) (rgb >> 24));
-	        }
-	    }
-	    buffer.flip();
-	    int textureID = GL11.glGenTextures();
-	    GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
-	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
-	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
-	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-	    GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, allowAlpha ? GL11.GL_RGBA8 : GL11.GL_RGB8, source.getWidth(), source.getHeight(), 0, allowAlpha ? GL11.GL_RGBA : GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, buffer);
-	    textures.add(textureID);
-	    return new GLGuiTexture(textureID);
+		if (isPowerOf2(source.getWidth()) && isPowerOf2(source.getHeight())) {
+			boolean allowAlpha = source.getTransparency() != BufferedImage.OPAQUE;
+			int effWidth = maxX - minX + 1;
+			int effHeight = maxY - minY + 1;
+			ByteBuffer buffer = BufferUtils.createByteBuffer(effWidth * effHeight * (allowAlpha ? 4 : 3)); //4 for RGBA, 3 for RGB
+	    	for(int y = minY; y <= maxY; y++){
+	        	for(int x = minX; x <= maxX; x++){
+	        		int rgb = source.getRGB(x, y);
+	        		buffer.put((byte) (rgb >> 16));
+	        		buffer.put((byte) (rgb >> 8));
+	        		buffer.put((byte) (rgb >> 0));
+	        		if(allowAlpha)
+	        			buffer.put((byte) (rgb >> 24));
+	        	}
+	    	}
+	    	buffer.flip();
+	    	int textureID = GL11.glGenTextures();
+	    	GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
+	    	GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+	    	GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+	    	GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+	    	GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+	    	GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, allowAlpha ? GL11.GL_RGBA8 : GL11.GL_RGB8, effWidth, effHeight, 0, allowAlpha ? GL11.GL_RGBA : GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, buffer);
+	    	textures.add(textureID);
+	    	return new GLGuiTexture(textureID);
+		} else {
+			BufferedImage image2 = new BufferedImage(next2Power(source.getWidth()), next2Power(source.getHeight()), source.getType());
+			return loadTexture(image2, minX, minY, maxX, maxY);
+		}
 	}
 
 	@Override
 	public GuiTexture loadTexture(BufferedImage source) {
-		boolean allowAlpha = source.getTransparency() != BufferedImage.OPAQUE;
-		ByteBuffer buffer = BufferUtils.createByteBuffer(source.getWidth() * source.getHeight() * (allowAlpha ? 4 : 3)); //4 for RGBA, 3 for RGB
-	    for(int y = 0; y < source.getHeight(); y++){
-	        for(int x = 0; x < source.getWidth(); x++){
-	        	int rgb = source.getRGB(x, y);
-	        	buffer.put((byte) (rgb >> 16));
-	        	buffer.put((byte) (rgb >> 8));
-	        	buffer.put((byte) (rgb >> 0));
-	        	if(allowAlpha)
-	        		buffer.put((byte) (rgb >> 24));
-	        }
-	    }
-	    buffer.flip();
-	    int textureID = GL11.glGenTextures();
-	    GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
-	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
-	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
-	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-	    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-	    GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, allowAlpha ? GL11.GL_RGBA8 : GL11.GL_RGB8, source.getWidth(), source.getHeight(), 0, allowAlpha ? GL11.GL_RGBA : GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, buffer);
-	    textures.add(textureID);
-	    return new GLGuiTexture(textureID);
+		if (isPowerOf2(source.getWidth()) && isPowerOf2(source.getHeight())) {
+			boolean allowAlpha = source.getTransparency() != BufferedImage.OPAQUE;
+			ByteBuffer buffer = BufferUtils.createByteBuffer(source.getWidth() * source.getHeight() * (allowAlpha ? 4 : 3)); //4 for RGBA, 3 for RGB
+	    	for(int y = 0; y < source.getHeight(); y++){
+	        	for(int x = 0; x < source.getWidth(); x++){
+	        		int rgb = source.getRGB(x, y);
+	        		buffer.put((byte) (rgb >> 16));
+	        		buffer.put((byte) (rgb >> 8));
+	        		buffer.put((byte) (rgb >> 0));
+	        		if(allowAlpha)
+	        			buffer.put((byte) (rgb >> 24));
+	        	}
+	    	}
+	    	buffer.flip();
+	    	int textureID = GL11.glGenTextures();
+	    	GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
+	    	GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+	    	GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+	    	GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+	    	GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+	    	GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, allowAlpha ? GL11.GL_RGBA8 : GL11.GL_RGB8, source.getWidth(), source.getHeight(), 0, allowAlpha ? GL11.GL_RGBA : GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, buffer);
+	    	textures.add(textureID);
+	    	return new GLGuiTexture(textureID);
+		} else {
+			BufferedImage image2 = new BufferedImage(next2Power(source.getWidth()), next2Power(source.getHeight()), source.getType());
+			return loadTexture(image2, 0, 0, source.getWidth() - 1, source.getHeight() - 1);
+		}
 	}
 
 	@Override
